7913fac6a13c6e35348db7f4d7395a9e76368f75
Jonas Bärtsch <jonas@pliik.io>
2021-01-01 11:05:38 +0100

    installed simpleanalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
 import "tailwindcss/tailwind.css";
+import Analytics from "analytics";
+import simpleAnalyticsPlugin from "analytics-plugin-simple-analytics";
+
+const analytics = Analytics({
+  app: "elisabethvonsteinen.ch",
+  plugins: [
+    // Load simple analytics! 🎉
+    simpleAnalyticsPlugin(),
+  ],
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
