c81e4893ab2ceab93c03b1fe2b2aa76acf3fa633
Jonas Bärtsch <jonas@pliik.io>
2021-01-01 09:00:06 +0100

    Content

---
+++ components/profile.js
@@ -0,0 +1,65 @@
+import Image from "next/image";
+export default function Profile() {
+  return (
+    <div className="my-8 space-y-12 lg:grid lg:grid-cols-1 lg:gap-8 lg:space-y-0">
+      <div className="lg:col-span-2">
+        <ul className="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0">
+          <li className="sm:py-8">
+            <div className="space-y-4 sm:grid sm:grid-cols-2 sm:items-start sm:gap-6 sm:space-y-0">
+              {/* Image */}
+              <div className="aspect-w-1 aspect-h-1 sm:aspect-w-1 sm:aspect-h-1">
+                <Image
+                  className="object-cover rounded-full"
+                  src="/profil.jpeg"
+                  width="100"
+                  height="100"
+                  alt
+                />
+              </div>
+              <div className="sm:col-span-2">
+                <div className="space-y-4">
+                  <div className="mt-0 prose prose-indigo text-gray-700 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+                    <p>
+                      Elisabeth von Steinen lebt in der Nähe von Zürich und in
+                      La Chaux-de-Fonds. Lange Jahre verbrachte sie in den
+                      Niederlanden, wo sie die flämische und niederländische
+                      Literatur zum Schreiben eigener Texte inspirierte.
+                      Elisabeth von Steinen begann, sich auf ihre Wurzeln zu
+                      besinnen. Zunächst erforschte sie die Sagen aus ihrer
+                      ersten Heimat. Daraus entspann sich ein dichter, fiktiver
+                      Text. Der Roman Zerrissene Zeit ist die erste
+                      Veröffentlichung von Elisabeth von Steinen.
+                    </p>
+                  </div>
+                  <ul className="flex space-x-5">
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/lisa-b%C3%A4rtsch-joosten-a13a53116/"
+                        className="text-gray-400 hover:text-gray-500"
+                      >
+                        <span>LinkedIn</span>
+                        <svg
+                          className="w-5 h-5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </li>
+          {/* More people...*/}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,104 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import Head from "next/head";
+import Image from "next/image";
+import Profile from "../components/profile";
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <>
+      <div className="bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
+          <div className="mx-auto text-base max-w-prose lg:max-w-none">
+            <h2 className="text-base text-gray-900 font-semibold tracking-wide uppercase">
+              Elisabeth von Steinen
+            </h2>
+            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Zerrissene Zeit
+            </h3>
+          </div>
+          <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+            <div className="relative lg:row-start-1 lg:col-start-2">
+              <svg
+                className="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20"
+                width={404}
+                height={384}
+                fill="none"
+                viewBox="0 0 404 384"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={384}
+                  fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
+                />
+              </svg>
+              <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
+                <figure>
+                  <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
+                    <Image
+                      className="rounded-lg shadow-lg object-cover object-center"
+                      src="/cover.jpeg"
+                      alt="Buchcover Zerrissene Zeit"
+                      width={375}
+                      height={600}
+                    />
+                  </div>
+                  <figcaption className="mt-3 flex text-sm text-gray-500">
+                    <span className="ml-2">
+                      Bild Schloss (Pastellkreiden auf Karton): Josef Müller
+                      (1886-1964) Mels <br />
+                      Foto Schloss: © Mike Kieme, Elikya Images
+                    </span>
+                  </figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="mt-8 lg:mt-0 font-serif">
+              <div className="text-base max-w-prose mx-auto lg:max-w-none">
+                <p className="text-lg text-gray-700">
+                  In diesem spannenden Roman mit Mystery-Elementen erkundet die
+                  mutige Eva auf ihren einsamen Streifzügen die Herkunft ihrer
+                  schüchternen Freundin Johanna. Dabei begegnet sie urwüchsigen
+                  Sagengestalten aus längst vergangenen Zeiten. Um den wahren
+                  Kern von Johannas Geschichte aufzuspüren, schlüpft Eva in
+                  einem leerstehenden, geheimnisvollen Haus durch den Riss in
+                  der Zeit. Johanna verliert durch ihre Rückzüge in ihr bewegtes
+                  Innenleben zunehmend den Bezug zur Realität. Erst nach Evas
+                  Verschwinden rafft sie sich auf und reist auf den Spuren ihrer
+                  Geschichte nach Leipzig, wo sie den Musiker Anton trifft. Er
+                  konfrontiert Johanna mit der Wirklichkeit und schenkt ihr die
+                  Geschichte einer grossen Freundschaft.
+                </p>
+                <a
+                  className="my-2 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  href="https://www.orellfuessli.ch/shop/home/artikeldetails/ID148073652.html"
+                >
+                  eBook Kaufen
+                </a>
+              </div>
+              <Profile />
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
